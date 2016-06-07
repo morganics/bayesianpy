96621b08d98887d96b4f6b3de10da43aa8f92c36
Ian <ianmorgan@outlook.com>
2016-06-07 12:08:35 +0100

    Adding binaries

--- bayespy/jni.py
+++ bayespy/jni.py
@@ -2,8 +2,8 @@ import jpype as jp
 import os
 
 base = os.path.dirname(os.path.realpath('__file__'))
-_classpath = ".;{};{}".format(os.path.join(base, 'bayespy/lib/bayesserver-7.6.jar'),
-                              os.path.join(base, 'bayespy/lib/sqlite-jdbc-3.8.11.2.jar'))
+_classpath = ".;{};{}".format(os.path.join(base, 'bayespy/bin/bayesserver-7.6.jar'),
+                              os.path.join(base, 'bayespy/bin/sqlite-jdbc-3.8.11.2.jar'))
 print(_classpath)
 if not jp.isJVMStarted():
     jp.startJVM(jp.getDefaultJVMPath(), "-Djava.class.path=%s" % _classpath)
