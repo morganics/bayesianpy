d1576602733c0b8ec2996894a2b545db71aabcb1
Ian <ianmorgan@outlook.com>
2016-07-20 16:36:13 +0100

    removing 'visual' lib has haven't got the matplotlib dependency working

--- bayespy/__init__.py
+++ bayespy/__init__.py
@@ -3,7 +3,7 @@ from bayespy import insight
 #from bayespy import ml
 from bayespy import model
 from bayespy import network
-from bayespy import visual
+#from bayespy import visual
 from bayespy.jni import bayesServer as _bs
 from bayespy import utils
 
