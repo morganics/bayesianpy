8453e750c6544efc2727fbf014956c409b8e43ab
Ian <ianmorgan@outlook.com>
2016-08-17 15:02:33 +0100

    Changing reference to inference lib..

--- bayespy/jni.py
+++ bayespy/jni.py
@@ -11,8 +11,8 @@ if platform.system() == "Linux":
 classpath = ".{0}{1}{0}{2}".format(separator, os.path.join(path_to_package, 'bin/bayesserver-7.6.jar'),
                               os.path.join(path_to_package, 'bin/sqlite-jdbc-3.8.11.2.jar'))
 if not jp.isJVMStarted():
-    jp.startJVM(jp.getDefaultJVMPath(), "-Djava.class.path=%s" % classpath)
+    jp.startJVM(jp.getDefaultJVMPath(), "-Djava.class.path={}".format(classpath))
     bayesServer = jp.JPackage("com.bayesserver")
     sqlLite = jp.JPackage("org.sqlite.JDBC")
-    bayesServerInference = bayesServer.inference
+    bayesServerInference = jp.JPackage("com.bayesserver.inference")
     bayesServerParams = jp.JPackage("com.bayesserver.learning.parameters")
