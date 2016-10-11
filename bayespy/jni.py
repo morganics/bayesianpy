db17b060693e0860f8ae018eeaa78445a19f12d1
Ian <ianmorgan@outlook.com>
2016-10-11 16:54:18 +0100

    Putting everything inside the 'isjvmstarted' block

--- bayespy/jni.py
+++ bayespy/jni.py
@@ -3,14 +3,15 @@ import os
 import bayespy.utils
 import platform
 
-path_to_package = bayespy.utils.get_path_to_parent_dir(__file__)
-separator = ";"
-if platform.system() == "Linux":
-    separator = ":"
-
-classpath = ".{0}{1}{0}{2}".format(separator, os.path.join(path_to_package, 'bin/bayesserver-7.8.jar'),
-                              os.path.join(path_to_package, 'bin/sqlite-jdbc-3.8.11.2.jar'))
 if not jp.isJVMStarted():
+    path_to_package = bayespy.utils.get_path_to_parent_dir(__file__)
+    separator = ";"
+    if platform.system() == "Linux":
+        separator = ":"
+
+    classpath = ".{0}{1}{0}{2}".format(separator, os.path.join(path_to_package, 'bin/bayesserver-7.8.jar'),
+                                       os.path.join(path_to_package, 'bin/sqlite-jdbc-3.8.11.2.jar'))
+
     jp.startJVM(jp.getDefaultJVMPath(), "-Djava.class.path={}".format(classpath), "-XX:-UseGCOverheadLimit", "-Xmx6g")
 
     # so it doesn't crash if called by a Python thread.
