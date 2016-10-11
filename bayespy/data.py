718be40d9f43a54c2f68a6aa75fdce681aa67c2e
Ian <ianmorgan@outlook.com>
2016-10-11 16:53:59 +0100

    Small changes to dataframereader and adding a few staticmethods

--- bayespy/data.py
+++ bayespy/data.py
@@ -1,5 +1,7 @@
 import numpy as np
 from bayespy.jni import *
+import logging
+import pandas as pd
 
 class DataFrameReader:
     def __init__(self, df):
@@ -25,6 +27,9 @@ class DataFrameReader:
 
     def to_dict(self):
         return self.row()
+
+    def tolist(self, cols):
+        return [self.__getitem__(c) for c in cols]
 
     def row(self):
         return {c : self.__getitem__(c) for c in self._columns}
@@ -77,21 +82,27 @@ class AutoType:
                 yield col
 
 class DataFrame:
+    @staticmethod
     def is_timestamp(dtype):
         return str(dtype) in ['timestamp64', 'timedelta64']
 
+    @staticmethod
     def is_float(dtype):
         return str(dtype) in {"float32", "float64"}
 
+    @staticmethod
     def is_int(dtype):
         return str(dtype) in {"int32", "int64"}
 
+    @staticmethod
     def is_bool(dtype):
         return str(dtype) == "bool"
 
+    @staticmethod
     def is_string(dtype):
         return str(dtype) == "object" or str(dtype) == "O"
 
+    @staticmethod
     def could_be_int(col):
         if DataFrame.is_int(col.dtype):
             return True
@@ -105,7 +116,8 @@ class DataFrame:
 
         return False
 
-    def coerce_to_numeric(df, logger, cutoff=0.10, ignore=[]):
+    @staticmethod
+    def coerce_to_numeric(df: pd.DataFrame, logger: logging.Logger, cutoff=0.10, ignore=[]):
         for col in df.columns:
             if col in ignore:
                 continue
@@ -122,6 +134,7 @@ class DataFrame:
 
         return df
 
+    @staticmethod
     def cast2(dtype, value):
         if DataFrame.is_int(dtype):
             return int(value)
@@ -132,6 +145,7 @@ class DataFrame:
 
         return value
 
+    @staticmethod
     def cast(df, col_name, value):
         if DataFrame.is_int(df[col_name].dtype):
             return int(value)
@@ -142,35 +156,25 @@ class DataFrame:
 
         return value
 
-def coerce_float_to_int(df):
-    for c in df.columns:
-        if not DataFrame.is_float(df[c].dtype):
-            continue
-
-        can_convert = True
-        for value in df[c].unique():
-            if np.isnan(value) or int(value) != value: #no nan for int cols it seems!
-                can_convert = False
-                break
-
-        if can_convert:
-            df[c] = df[c].astype(int)
-
 class Filter:
-    def remove_static_variables(df):
+    @staticmethod
+    def remove_static_variables(df: pd.DataFrame):
         column_names = df.apply(lambda x: len(x.unique()) > 1)
-        return df[column_names[column_names == True].index.tolist()]
+        return df[column_names[column_names == True].index.tolist()].copy()
 
-    def remove_variable_variables(df):
+    @staticmethod
+    def remove_variable_variables(df: pd.DataFrame):
         column_names = df.apply(lambda x: len(x.unique()) != len(df))
         return df[column_names[column_names == True].index.tolist()]
 
-    def remove_discrete_variables_with_too_many_states(df, num_states = 30):
+    @staticmethod
+    def remove_discrete_variables_with_too_many_states(df: pd.DataFrame, num_states = 30):
         column_names = df.select_dtypes(include=['object']).apply(lambda x: len(x.unique()) >= num_states)
         cols = list(set(df.columns.tolist()) - set(column_names[column_names == True].index.tolist()))
         return df[cols]
 
-    def apply(df):
+    @staticmethod
+    def apply(df: pd.DataFrame):
         return Filter.remove_discrete_variables_with_too_many_states(Filter.remove_variable_variables(Filter.remove_static_variables(df)))
 
 def create_histogram(series):
@@ -178,7 +182,7 @@ def create_histogram(series):
     values = []
     for value in series.iteritems():
         v = bayesServerDiscovery.WeightedValue()
-        if np.isnan(value[1]):
+        if np.isnan(value[1]) or np.isinf(value[1]):
             continue
         v.setValue(jp.java.lang.Double(value[1]))
         v.setWeight(1.0)
