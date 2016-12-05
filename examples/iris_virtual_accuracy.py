f94b5579c548576386b59c286d9c8bd65a1554b4
Ian <ianmorgan@outlook.com>
2016-12-05 09:48:19 +0000

    Adding a few comments

--- examples/iris_virtual_accuracy.py
+++ examples/iris_virtual_accuracy.py
@@ -7,9 +7,6 @@ import os
 
 from bayespy.jni import jp
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score
-
-import numpy as np
 
 import matplotlib.pyplot as plt
 
@@ -35,19 +32,27 @@ def main():
 
     train, test = train_test_split(iris, test_size=0.5)
 
+    # train the model and query the most likely states and probability of each latent state.
     with bayespy.data.DataSet(iris, db_folder, logger) as dataset:
         model = bayespy.model.NetworkModel(network, logger)
         model.train(dataset.subset(train.index.tolist()))
 
-        subset = dataset.subset(test.index.tolist())
+        test_subset = dataset.subset(test.index.tolist())
 
-        results = model.batch_query(subset,
+        results = model.batch_query(test_subset,
+                                    # creates columns Cluster$$Cluster0, Cluster$$Cluster1,
+                                    # Cluster$$Cluster2, as
+                                    # suffix is set to an empty string.
                                     [bayespy.model.QueryStateProbability("Cluster", suffix=""),
+                                     # creates column 'iris_class_maxlikelihood'
                                      bayespy.model.QueryMostLikelyState("iris_class"),
+                                     # creates column 'Cluster_maxlikelihood'
                                      bayespy.model.QueryMostLikelyState("Cluster")
                                      ])
 
     cluster_accuracy = {}
+    # get a list of cluster accuracies, using the Bayes Server Confusion matrix class
+    # weighted by the Cluster accuracy.
     with bayespy.data.DataSet(results, db_folder, logger) as resultset:
         for c in range(num_clusters):
             matrix = bayespy.jni.bayesServerAnalysis()\
@@ -55,7 +60,7 @@ def main():
                                         "iris_class_maxlikelihood", "Cluster$$Cluster{}".format(c))
             cluster_accuracy.update({'Cluster{}'.format(c) : matrix.getAccuracy()})
 
-
+    # generate samples from the trained model, to give us some additional testing data.
     samples = bayespy.model.Sampling(network).sample(num_samples=20).drop(["Cluster", "iris_class"], axis=1)
     reader = bayespy.data.DataFrameReader(samples)
     inference = bayespy.model.InferenceEngine(network).create_engine()
@@ -63,6 +68,8 @@ def main():
     query = bayespy.model.SingleQuery(network, inference, logger)
     query_type = [bayespy.model.QueryStateProbability('Cluster', suffix="")]
 
+    # query the expected Cluster membership, and generate a wrapper for
+    # comparing the values, weighted by cluster membership.
     while reader.read():
         result = query.query(query_type, evidence=evidence.apply(reader.to_dict()))
         cv_results = []
@@ -73,17 +80,19 @@ def main():
                 jp.JDouble(weighting), jp.JObject(value, jp.java.lang.Object), jp.java.lang.Double(jp.JDouble(value)))
             )
 
+
         score = bayespy.jni.bayesServerAnalysis().CrossValidation.combine(jp.java.util.Arrays.asList(cv_results),
                                     bayespy.jni.bayesServerAnalysis().CrossValidationCombineMethod.WEIGHTED_AVERAGE)
 
+        # append the score on to the existing dataframe
         samples.set_value(reader.get_index(), 'score', score)
 
     variables = ['sepal_length', 'sepal_width', 'petal_length', 'petal_width']
-    #
+
     cmap = plt.cm.get_cmap('Blues')
     fig = plt.figure(figsize=(10, 10))
     k = 1
-    #
+    # plot!
     for i, v in enumerate(variables):
         for j in range(i + 1, len(variables)):
             v1 = variables[j]
