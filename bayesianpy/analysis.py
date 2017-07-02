edcd5c0c77e7964b640a453afd58e9064d4925d2
Ian Morgan <Ian Morgan>
2017-07-02 10:37:53 +0100

    Adding KFoldAnalysis class, to make kfold stuff a bit easier

--- bayesianpy/analysis.py
+++ bayesianpy/analysis.py
@@ -6,6 +6,8 @@ import numpy as np
 import logging
 from typing import Iterable, List
 from collections import defaultdict
+import os
+from sklearn.model_selection import train_test_split
 
 from sklearn.metrics import r2_score
 def continuous_score(x, y):
@@ -16,15 +18,78 @@ def discrete_score(x, y):
     return accuracy_score(x, y['MaxStateLikelihood'])
 
 from sklearn.metrics import confusion_matrix
-def fmeasure_score(predicted, actual):
-    return _fmeasure(*confusion_matrix(predicted, actual).flatten())
+def fmeasure_score(predicted, actual, labels=None):
+    return _fmeasure(*confusion_matrix(actual, predicted, labels=labels).flatten())
 
 def _fmeasure(tp, fp, fn, tn):
     """Computes effectiveness measures given a confusion matrix."""
     specificity = tn / (tn + fp)
     sensitivity = tp / (tp + fn)
     fmeasure = 2 * (specificity * sensitivity) / (specificity + sensitivity)
-    return { 'sensitivity': sensitivity, 'specificity': specificity, 'fmeasure': fmeasure }
+    return { 'sensitivity': sensitivity, 'specificity': specificity, 'precision': tp / (tp + fp),
+        'fmeasure': fmeasure,
+             'tp': tp, 'fp': fp, 'fn': fn, 'tn': tn,
+             'positive_likelihood_ratio': sensitivity / (1 - specificity),
+             'negative_likelihood_ratio': (1- sensitivity) / specificity}
+
+def predictive_value(predicted, actual, labels=None):
+    def _predictive_value(tp, fp, fn, tn):
+        return {'positive_predictive_value': tp / (tp + fp),
+            'negative_predictive_value': tn/(tn + fn)}
+
+    return _predictive_value(*confusion_matrix(predicted, actual, labels=labels).flatten())
+
+from bayesianpy.network import Builder as builder
+
+class DiscretisationAnalysis:
+
+    def __init__(self, logger, shuffle=True):
+        self._shuffle = shuffle
+        self._logger = logger
+
+    def analyse(self, df: pd.DataFrame, continuous_variable_names:List[str]):
+        kf = KFold(df.shape[0], n_folds=3, shuffle=self._shuffle)
+
+        network_factory = bayesianpy.network.NetworkFactory(self._logger)
+        variations = [1,5,10,20,30]
+        results = {}
+        with bayesianpy.data.DataSet(df, logger=self._logger) as dataset:
+            ll = defaultdict(list)
+            for variable in continuous_variable_names:
+                likelihoods = []
+                for cluster_count in variations:
+                    weighted = []
+                    weights = []
+                    for k, (train_indexes, test_indexes) in enumerate(kf):
+
+
+                        x_train, x_test = train_indexes, test_indexes
+
+                        nt = network_factory.create()
+                        cluster = builder.create_cluster_variable(nt, cluster_count)
+                        node = builder.create_continuous_variable(nt, variable)
+                        builder.create_link(nt, cluster, node)
+
+                        model = bayesianpy.model.NetworkModel(nt, self._logger)
+
+                        try:
+                            ll = model.train(dataset.subset(x_train)).get_metrics()['loglikelihood']
+                        except BaseException as e:
+                            self._logger.warning(e)
+                            continue
+
+                        weighted.append(ll)
+                        weights.append(len(x_train))
+
+                    likelihoods.append(np.average(weighted, weights=weights))
+
+                max_index = np.argmax(likelihoods)
+                if variations[max_index] > 5:
+                    results.update({variable: True})
+                else:
+                    results.update({variable: False})
+
+        return results
 
 class LogLikelihoodAnalysis:
     """
@@ -65,6 +130,43 @@ class LogLikelihoodAnalysis:
 
         return pd.DataFrame(ll)
 
+class KFoldAnalysis:
+    """
+    Used for comparing models
+    """
+
+    def __init__(self, logger, shuffle=True):
+        self._shuffle = shuffle
+        self._logger = logger
+        self._models = []
+
+    def get_models(self) -> List[bayesianpy.model.NetworkModel]:
+        return self._models
+
+    def analyse(self, df: pd.DataFrame, tpl: bayesianpy.template.Template, dataset, queries, k=3, append_to_df=True):
+        if k > 1:
+            kf = KFold(df.shape[0], n_folds=k, shuffle=self._shuffle)
+        else:
+            self._logger.info("Not KFold anymore, just doing a train/test split.")
+            x_train, x_test = train_test_split(df, test_size=0.33)
+            kf = [(x_train.index, x_test.index)]
+
+        network_factory = bayesianpy.network.NetworkFactory(self._logger)
+
+        for k, (x_train, x_test) in enumerate(kf):
+
+            self._logger.info("Running KFold {} of {}".format(k, len(kf)))
+            model = bayesianpy.model.NetworkModel(tpl.create(network_factory), self._logger)
+            self._models.append(model)
+            try:
+                model.train(dataset.subset(x_train))
+            except BaseException as e:
+                self._logger.warning(e)
+                continue
+
+            model.save("trained.bayes")
+            yield model.batch_query(dataset.subset(x_test), queries,
+                                      append_to_df=append_to_df)
 
 # class RegressionAnalysis:
 #
