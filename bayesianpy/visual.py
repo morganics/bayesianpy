35a22286785ae827b9e01d377dc8db7cf5fb8bdd
Ian Morgan <Ian Morgan>
2017-08-24 11:13:40 +0100

    Adding visualisation for cumulative gains chart

--- bayesianpy/visual.py
+++ bayesianpy/visual.py
@@ -8,6 +8,7 @@ import pandas as pd
 import math
 import scipy.stats as ss
 from typing import List, Dict
+import sklearn.metrics
 
 import logging
 
@@ -204,3 +205,113 @@ class JointDistribution:
                                         (head_variables[i], head_variables[j]), results)
 
         yield fig
+
+from matplotlib import pyplot as plt
+import itertools
+def plot_confusion_matrix(cm, classes,
+                          normalize=False,
+                          title='Confusion matrix',
+                          cmap=plt.cm.Blues):
+
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    if normalize:
+        real_values = cm
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        real_values = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print('Confusion matrix, without normalization')
+
+    print(cm)
+
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, "{:0.2f} ({:0.2f})".format(cm[i, j], real_values[i,j]),
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+
+    plt.tight_layout()
+    plt.ylabel('Actual')
+    plt.xlabel('Predicted')
+
+def _split_df(df, actual_col, predicted_col):
+    rows = []
+    for group in np.array_split(df, 10):
+        score = sklearn.metrics.accuracy_score(group[actual_col].tolist(),
+                                               group[predicted_col].tolist(),
+                                               normalize=False)
+
+        rows.append({'NumCases': len(group), 'NumCorrectPredictions': score})
+
+    return pd.DataFrame(rows)
+
+def calc_cumulative_gains(df: pd.DataFrame, actual_col: str, predicted_col:str, probability_col:str):
+    df.sort_values(by=probability_col, ascending=True, inplace=True)
+
+    subset = df[df[predicted_col] == True]
+    lift = _split_df(subset, actual_col, predicted_col)
+
+    #Cumulative Gains Calculation
+    lift['RunningCorrect'] = lift['NumCorrectPredictions'].cumsum()
+    lift['PercentCorrect'] = lift.apply(
+        lambda x: (100 / lift['NumCorrectPredictions'].sum()) * x['RunningCorrect'], axis=1)
+    lift['CumulativeCorrectBestCase'] = lift['NumCases'].cumsum()
+    lift['PercentCorrectBestCase'] = lift['CumulativeCorrectBestCase'].apply(
+        lambda x: 100 if (100 / lift['NumCorrectPredictions'].sum()) * x > 100 else (100 / lift[
+            'NumCorrectPredictions'].sum()) * x)
+    lift['AvgCase'] = lift['NumCorrectPredictions'].sum() / len(lift)
+    lift['CumulativeAvgCase'] = lift['AvgCase'].cumsum()
+    lift['PercentAvgCase'] = lift['CumulativeAvgCase'].apply(
+        lambda x: (100 / lift['NumCorrectPredictions'].sum()) * x)
+
+    #Lift Chart
+    lift['NormalisedPercentAvg'] = 1
+    lift['NormalisedPercentWithModel'] = lift['PercentCorrect'] / lift['PercentAvgCase']
+
+    return lift
+
+def plot_binned_response_rate(lift: pd.DataFrame):
+    import seaborn as sns
+    plt.figure()
+    sns.barplot(y=lift['NumCorrectPredictions'] / lift['NumCases'], x=lift.index.tolist(), color='salmon', saturation=0.5)
+    plt.show()
+
+def plot_cumulative_gains(lift: pd.DataFrame):
+    fig, ax = plt.subplots()
+    fig.canvas.draw()
+
+    handles = []
+    handles.append(ax.plot(lift['PercentCorrect'], 'r-', label='Percent Correct Predictions'))
+    handles.append(ax.plot(lift['PercentCorrectBestCase'], 'g-', label='Best Case (for current model)'))
+    handles.append(ax.plot(lift['PercentAvgCase'], 'b-', label='Average Case (for current model)'))
+    ax.set_xlabel('Total Population (%)')
+    ax.set_ylabel('Number of Respondents (%)')
+
+    ax.set_xlim([0, 9])
+    ax.set_ylim([10, 100])
+    try:
+        labels = [int((label+1)*10) for label in [float(item.get_text()) for item in ax.get_xticklabels() if len(item.get_text()) > 0]]
+    except BaseException as e:
+        print([item.get_text() for item in ax.get_xticklabels()])
+
+    ax.set_xticklabels(labels)
+
+    fig.legend(handles, labels=[h[0].get_label() for h in handles])
+    fig.show()
+
+def plot_lift_chart(lift: pd.DataFrame):
+    plt.figure()
+    plt.plot(lift['NormalisedPercentAvg'], 'r-', label='Normalised \'response rate\' with no model')
+    plt.plot(lift['NormalisedPercentWithModel'], 'g-', label='Normalised \'response rate\' with using model')
+    plt.legend()
+    plt.show()
