9b929495f88ad068b60bf1b904e08cd9cf3d031c
Ian <ianmorgan@outlook.com>
2018-03-04 13:59:30 +0000

    adding a synchronous mode to the daskpool

--- bayesianpy/distributed.py
+++ bayesianpy/distributed.py
@@ -4,14 +4,19 @@ import dask.dataframe as dd
 import dask.array as da
 import numpy as np
 import pathos.multiprocessing as mp
+import logging
 
 class DaskPool:
     '''
     We need to use DaskPool to avoid hanging in linux when multiprocessing is required. Jpype hangs
     when processes are not spawned.
     '''
-    def __init__(self, processes:int=None):
+    def __init__(self, processes:int=None, mode='multiprocessing'):
         self._processes = processes
+        self._logger = logging.getLogger(__name__)
+        self._mode = mode
+        self._pool = None
+        self._options = None
 
     def _calc_threads(self):
         if mp.cpu_count() == 1:
@@ -23,23 +28,33 @@ class DaskPool:
 
     def __enter__(self):
 
-        import dask.multiprocessing
-        import multiprocess.context as ctx
+        if self._mode == 'multiprocessing':
+            import dask.multiprocessing
+            import multiprocess.context as ctx
 
-        ctx._force_start_method('spawn')
+            ctx._force_start_method('spawn')
 
-        self._pool = mp.Pool(processes=self._calc_threads() if self._processes is None else self._processes)
-        self._options = dask.context.set_options(pool=self._pool)
+
+            processes = self._calc_threads() if self._processes is None else self._processes
+            self._pool = mp.Pool(processes=processes)
+            self._options = dask.context.set_options(pool=self._pool, get=dask.multiprocessing.get)
+            self._logger.info("Starting processing with {} processes".format(processes))
+        elif self._mode == 'synchronous':
+            import dask.local
+            self._options = dask.context.set_options(get=dask.local.get_sync)
+            self._logger.info("Starting synchronous processing")
 
         return self
 
-    def get(self):
-        import dask.multiprocessing
-        return dask.multiprocessing.get
-
     def __exit__(self, exc_type, exc_val, exc_tb):
-        self._options.__exit__(exc_type, exc_val, exc_tb)
-        self._pool.__exit__(exc_type, exc_val, exc_tb)
+        if self._options is not None:
+            self._options.__exit__(exc_type, exc_val, exc_tb)
+
+        if self._pool is not None:
+            self._pool.__exit__(exc_type, exc_val, exc_tb)
+
+        self._options = None
+        self._pool = None
 
 def compute(df):
     if hasattr(df, 'compute'):
@@ -95,5 +110,6 @@ def to_sql(df, table, engine, index_label='ix', index=True, if_exists=None):
         mode = 'append'
         if if_exists is not None:
             mode = if_exists
+
         for partition in _get_df_partitions(df):
             partition.to_sql(table, engine, if_exists=mode, index_label=index_label, index=index)
