23e3d879fd19517691537ed866cf3b0b1ec6f9c5
Ian <ianmorgan@outlook.com>
2018-03-04 14:02:24 +0000

    Adding requirements for installation

--- setup.py
+++ setup.py
@@ -9,6 +9,12 @@ setup(
     author='morganics',
     author_email='',
     description='', 
-	install_requires=['pandas', 'sqlalchemy', 'networkx', 'numpy', 'jpype1', 'pathos', 'scikit-learn', 'dask',
-                      'multiprocess']#, 'matplotlib', 'seaborn']
+	install_requires=['pandas', 'sqlalchemy',
+                      'numpy',
+                      'jpype1>=0.6.1', 'pathos', 'scikit-learn',
+                      'scipy',
+                      'dask[complete]==0.16',
+                      'multiprocess'
+                      ],
+    include_package_data=True
 )
